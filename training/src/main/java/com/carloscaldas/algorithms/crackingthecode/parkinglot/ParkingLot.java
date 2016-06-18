2a5975898f60a9c89134e7e92a0323a184c26dad
Carlos Caldas <Carlos Caldas>
2016-06-18 17:58:48 -0300

    reestruturacao

--- training/src/main/java/com/carloscaldas/algorithms/crackingthecode/parkinglot/ParkingLot.java
+++
@@ -1,5 +0,0 @@
-package com.carloscaldas.algorithms.crackingthecode.parkinglot;
-
-public class ParkingLot {
-
-}
--- training/src/main/java/com/carloscaldas/algorithms/crackingthecode/parkinglot/VehicleSize.java
+++
@@ -1,6 +0,0 @@
-package com.carloscaldas.algorithms.crackingthecode.parkinglot;
-
-public enum VehicleSize {
-	Motorcycle, Compact, Large, Bike
-}
-
